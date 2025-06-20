ab335dca4e7dcec81fd1ba3254a64a9c4024f8a8
KANGPUNGYUN <zkdvnd@naver.com>
2025-06-20 23:09:02 +0900

    fix: 폰트 preload 설정

--- src/components/ui/Tag/TagItem.tsx
+++ src/components/ui/Tag/TagItem.tsx
@@ -62,7 +62,7 @@ const TagItem: React.FC<TagProps> = ({
       alignItems: "center" as const,
       gap: "10px",
       borderRadius: "999px",
-      fontFamily: "Gmarket Sans",
+      fontFamily: "gmarketSans",
       fontStyle: "normal" as const,
       fontWeight: 400,
       lineHeight: "135%",
