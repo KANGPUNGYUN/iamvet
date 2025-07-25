831d832ecb17d6cb380dcd12cdfa9a3e409b9561
KANGPUNGYUN <zkdvnd@naver.com>
2025-07-25 17:17:01 +0900

    feat: 폰트 위치 수정

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,17 +3,17 @@ import localFont from "next/font/local";
 export const gmarketSans = localFont({
   src: [
     {
-      path: "./fonts/title/GmarketSansLight.woff2",
+      path: "/fonts/title/GmarketSansLight.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/title/GmarketSansMedium.woff2",
+      path: "/fonts/title/GmarketSansMedium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/title/GmarketSansBold.woff2",
+      path: "/fonts/title/GmarketSansBold.woff2",
       weight: "700",
       style: "normal",
     },
@@ -21,61 +21,61 @@ export const gmarketSans = localFont({
   variable: "--font-title",
   display: "swap",
   fallback: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen',
-    'Ubuntu',
-    'Cantarell',
-    'sans-serif'
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "sans-serif",
   ],
 });
 
 export const suit = localFont({
   src: [
     {
-      path: "./fonts/text/SUIT-Thin.woff2",
+      path: "/fonts/text/SUIT-Thin.woff2", // ./ 제거하고 / 로 시작
       weight: "100",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-ExtraLight.woff2",
+      path: "/fonts/text/SUIT-ExtraLight.woff2",
       weight: "200",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-Light.woff2",
+      path: "/fonts/text/SUIT-Light.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-Regular.woff2",
+      path: "/fonts/text/SUIT-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-Medium.woff2",
+      path: "/fonts/text/SUIT-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-SemiBold.woff2",
+      path: "/fonts/text/SUIT-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-Bold.woff2",
+      path: "/fonts/text/SUIT-Bold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-ExtraBold.woff2",
+      path: "/fonts/text/SUIT-ExtraBold.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "./fonts/text/SUIT-Heavy.woff2",
+      path: "/fonts/text/SUIT-Heavy.woff2",
       weight: "900",
       style: "normal",
     },
@@ -83,13 +83,13 @@ export const suit = localFont({
   variable: "--font-text",
   display: "swap",
   fallback: [
-    '-apple-system',
-    'BlinkMacSystemFont',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen',
-    'Ubuntu',
-    'Cantarell',
-    'sans-serif'
+    "-apple-system",
+    "BlinkMacSystemFont",
+    "Segoe UI",
+    "Roboto",
+    "Oxygen",
+    "Ubuntu",
+    "Cantarell",
+    "sans-serif",
   ],
 });
