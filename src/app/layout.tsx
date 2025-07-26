77bdbb3dbf7f12b6b89eb73be85d9b4c9c51155a
KANGPUNGYUN <zkdvnd@naver.com>
2025-07-27 00:36:09 +0900

    fix: font-display swap에서 block로 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,48 @@ export default function RootLayout({
     <html lang="ko" className={`${gmarketSans.variable} ${suit.variable}`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="preload"
+          href="/fonts/title/GmarketSansLight.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/title/GmarketSansMedium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/title/GmarketSansBold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/text/SUIT-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/text/SUIT-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/fonts/text/SUIT-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
       </head>
       <body className="font-text">{children}</body>
     </html>
