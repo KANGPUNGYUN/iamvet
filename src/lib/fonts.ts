02d838829b2b5f7a4f6cf4ac878603654d7df8df
KANGPUNGYUN <zkdvnd@naver.com>
2025-07-27 01:52:32 +0900

    feat: 구글 폰트 테스트

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,75 +1,16 @@
-import localFont from 'next/font/local';
+import { Inter, Noto_Sans_KR } from 'next/font/google';
 
-export const gmarketSans = localFont({
-  src: [
-    {
-      path: '../app/fonts/title/GmarketSansLight.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/title/GmarketSansMedium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/title/GmarketSansBold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-  ],
+// 임시로 Google Fonts 사용하여 테스트
+export const gmarketSans = Noto_Sans_KR({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
   variable: '--font-title',
   display: 'swap',
 });
 
-export const suit = localFont({
-  src: [
-    {
-      path: '../app/fonts/text/SUIT-Thin.woff2',
-      weight: '100',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-ExtraLight.woff2',
-      weight: '200',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-Light.woff2',
-      weight: '300',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-Regular.woff2',
-      weight: '400',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-Medium.woff2',
-      weight: '500',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-SemiBold.woff2',
-      weight: '600',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-Bold.woff2',
-      weight: '700',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-ExtraBold.woff2',
-      weight: '800',
-      style: 'normal',
-    },
-    {
-      path: '../app/fonts/text/SUIT-Heavy.woff2',
-      weight: '900',
-      style: 'normal',
-    },
-  ],
+export const suit = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   variable: '--font-text',
   display: 'swap',
 });
