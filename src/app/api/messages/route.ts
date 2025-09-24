b591d888ef5332453222a84cfd3251e8ce299e09
KANGPUNGYUN <zkdvnd@naver.com>
2025-09-24 15:16:44 +0900

    feat: 메시지 에러 확인

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,8 +1,6 @@
 import { NextRequest } from "next/server";
 import { verifyToken } from "@/lib/auth";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/lib/prisma";
 
 export async function GET(request: NextRequest) {
   try {
